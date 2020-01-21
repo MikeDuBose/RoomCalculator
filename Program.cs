3b9d16c0d6bed7ec1463c365b07a9cdcc83e17fa
Michael DuBose <michaeldubose@quickenloans.com>
2020-01-21 15:44:28 -0500

    Finished lab 1.  Asking user for input and calculates the area, permiter, and volume.

--- Program.cs
+++ Program.cs
@@ -6,23 +6,38 @@ namespace RoomCalculator
     {
 
 
+        
+        //Method that calculates the area and makes the call to other methods
         private static double calculateArea()
         {
+            //Receive user input
             Console.Write("What is the length of this room? ");
             double length = double.Parse(Console.ReadLine());
             Console.Write("What is the width of this room? ");
             double width = double.Parse(Console.ReadLine());
+            Console.Write("What is the height of this room? ");
+            double height = double.Parse(Console.ReadLine());
             double area = width * length;
-            Console.WriteLine("The length of " + length + " and the width of " + width + " with an area of " + area);
+            Console.WriteLine("The room has an area of " + area + ".");
+            calculateVolume(length, width, height);
             calculatePerimeter(length, width);
             checkSize(area);
+            //This checks with the user to see if they would like to continue.
             proceed();
             return area;
         }
 
+        //Method that calculates the volume of the user input
+        private static double calculateVolume(double length, double width, double height)
+        {
+            double volume = length * width * height;
+            Console.WriteLine("The room has a volume of " + volume + ".");
+            return volume;
+        }
+
         private static void proceed()
         {
-            Console.Write("Would you like to proceed?  Y/N");
+            Console.Write("Would you like to proceed?  Y/N:  ");
             string response = Console.ReadLine();
             response = response.ToLower();
             if (response == "y")
@@ -59,7 +74,7 @@ namespace RoomCalculator
         {
 
             double perimeter = 2 * (length + width);
-            Console.WriteLine("The length of " + length + " and the width of " + width + " with a perimeter of " + perimeter);
+            Console.WriteLine("The room has a preimeter of " + perimeter + ".");
             return perimeter;
         }
 
@@ -67,7 +82,8 @@ namespace RoomCalculator
 
         static void Main(string[] args)
         {
-
+            //By placing this WriteLine here, I ensure that it is only called once and not every time calculateArea() is called.
+            Console.WriteLine("Hello and welcome to Room Sizer 2.0!  Now 50% less deadly than our last version!  Please answer the following questions: ");
             calculateArea();
             
             
